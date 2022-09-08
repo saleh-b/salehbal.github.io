b8b603df502e0768f6566689b632fb7a5f6dcc82
unknown <salehb.professional@gmail.com>
2022-09-08 06:01:01 +0400

    Swiching to fullPage.js

---
+++ main.js
@@ -0,0 +1,12 @@
+new fullpage('#fullpage', {
+  //options here
+  autoScrolling: true,
+  scrollHorizontally: true,
+  sectionsColor: ['#00a6fbff', '#0582caff', '#006494ff', '#003554ff', '#051923ff'],
+});
+// /* CSS HEX */
+// --blue-jeans: #00a6fbff;
+// --green-blue-crayola: #0582caff;
+// --sapphire-blue: #006494ff;
+// --prussian-blue: #003554ff;
+// --rich-black-fogra-29: #051923ff;
--- script.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.section').addEventListener('scroll', (e) => {
-  console.log('scroll');
-});
