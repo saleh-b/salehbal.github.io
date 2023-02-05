33a2c334bfa7090d4b5073e93530107e98a92205
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-02-05 12:45:23 +0400

    Update

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-new fullpage('#fullpage', {
-  //options here
-  autoScrolling: true,
-  scrollHorizontally: true,
-  sectionsColor: ['#23272E', '#1D2126'],
-  anchors: ['firstPage', 'secondPage', 'thirdPage'],
-});
-
 new TypeIt('#title_animation', {
   strings: ['Saleh Balakişiyev', 'Front end web dev.'],
-  // loop: true,
+  loop: true,
 }).go();
