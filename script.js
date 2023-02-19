97c26db1c285e660acd141ecd60bc46fdef33b2f
SalehBal <94306843+SalehBal@users.noreply.github.com>
2023-02-19 09:30:21 +0400

    Email form working

--- script.js
+++ script.js
@@ -3,30 +3,18 @@ new TypeIt('#title_animation', {
   loop: true,
 }).go();
 
-// function sendEmail() {
-//   console.log('Sent');
-//   Email.send({
-//     Host: 'smtp.elasticemail.com',
-//     Username: 'username',
-//     Password: 'password',
-//     To: 'salehb.professional@gmail.com',
-//     From: 'you@isp.com',
-//     Subject: 'This is the subject',
-//     Body: 'And this is the body',
-//   }).then((message) => alert(message));
-// }
+emailjs.init('cSd3aq2p237q1DKup');
 
-// $('#email_fom_submit').submit(function (event) {
-//   // console.log(event);
-//   debugger;
-//   event.preventDefault();
-//   sendEmail();
-
-//   Email.send({
-//     SecureToken: 'b70283a5-e3bc-4af1-b221-aa7c40a4d746',
-//     To: 'salehb.professional@gmail.com ',
-//     From: 'salehb.professional@gmail.com ',
-//     Subject: 'Portfolion website',
-//     Body: $('#'),
-//   }).then((message) => alert(message));
-// });
+$('#email_form_submit').submit(function (event) {
+  debugger;
+  event.preventDefault();
+  emailjs
+    .send('service_bqlcpje', 'template_jqa40mg', {
+      from_name: `Portfolio ${$('#name').val()}`,
+      email_id: $('#email').val(),
+      message: $('#message').val(),
+    })
+    .then(() => {
+      alert('Email successfully sent.');
+    });
+});
